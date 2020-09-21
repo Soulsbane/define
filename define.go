117d46c43621fd778d3340a116539b67b8bb7150
Paul Crane <paul@acheronsoft.com>
2020-09-21 01:49:23 -0500

    Add initial fetching of a words defnition.

--- define.go
+++ define.go
@@ -1,8 +1,15 @@
 package main
 
-import "github.com/alexflint/go-arg"
+import (
+	"encoding/json"
+	"fmt"
 
-type word struct {
+	"github.com/alexflint/go-arg"
+	"github.com/levigross/grequests"
+)
+
+// DictionaryObject Structure containing the values fetched from dictionaryapi.dev
+type DictionaryObject struct {
 	Meanings []struct {
 		Definitions []struct {
 			Definition string   `json:"definition"`
@@ -18,12 +25,35 @@ type word struct {
 	Word string `json:"word"`
 }
 
+func getDefinition(wordToFind string) {
+	resp, err := grequests.Get("https://api.dictionaryapi.dev/api/v2/entries/en/"+wordToFind, nil)
+
+	if err != nil {
+		fmt.Println("Failed to fetch word from dictionary API: ", err)
+	}
+
+	var dictionaryObject []DictionaryObject
+	err = json.Unmarshal(resp.Bytes(), &dictionaryObject)
+
+	if err != nil {
+		fmt.Println("Failed to find a definition for: " + wordToFind)
+	} else {
+		fmt.Println(dictionaryObject[0].Meanings[0].Definitions[0].Definition)
+	}
+
+}
+
 // https://api.dictionaryapi.dev/api/v2/entries/<language_code>/<word>
 // https://api.dictionaryapi.dev/api/v2/entries/en/hello
 // https://api.dictionaryapi.dev/api/v2/entries/ja/緑
 func main() {
 	var args struct {
+		Word string `arg:"positional, required"`
 	}
 
 	arg.MustParse(&args)
+
+	if args.Word != "" {
+		getDefinition(args.Word)
+	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/Soulsbane/define
 
 go 1.15
 
-require github.com/alexflint/go-arg v1.3.0
+require (
+	github.com/alexflint/go-arg v1.3.0
+	github.com/levigross/grequests v0.0.0-20190908174114-253788527a1a
+)
