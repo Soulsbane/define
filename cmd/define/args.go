3dc5ad9390191b5887818be67f06ac5bd5e35df9
Paul Crane <paul@acheronsoft.com>
2022-12-16 03:13:12 -0600

    Add version command line argument

--- cmd/define/args.go
+++ cmd/define/args.go
@@ -1,9 +1,19 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/carlmjohnson/versioninfo"
+)
+
 type ProgramArgs struct {
 	Word string `arg:"positional, required"`
 }
 
 func (args ProgramArgs) Description() string {
 	return "A simple command line application that finds the definition to a word"
+}
+
+func (ProgramArgs) Version() string {
+	return fmt.Sprintln("Version: ", versioninfo.Short())
 }
--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.15
 
 require (
 	github.com/alexflint/go-arg v1.3.0
+	github.com/carlmjohnson/versioninfo v0.22.4 // indirect
 	github.com/levigross/grequests v0.0.0-20190908174114-253788527a1a
 )
