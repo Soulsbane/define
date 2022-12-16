e277dcb7258d6a27f05b7105df7b88e8fb0b3034
Paul Crane <paul@acheronsoft.com>
2022-12-16 03:11:29 -0600

    Add description to CLI output

--- cmd/define/args.go
+++ cmd/define/args.go
@@ -3,3 +3,7 @@ package main
 type ProgramArgs struct {
 	Word string `arg:"positional, required"`
 }
+
+func (args ProgramArgs) Description() string {
+	return "A simple command line application that finds the definition to a word"
+}
